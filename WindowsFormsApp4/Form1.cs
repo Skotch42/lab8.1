f65e67995cfb9969526366641de83686e3a04b7a
Комардин Влад <99380380+Skotch42@users.noreply.github.com>
2023-05-05 21:01:21 +0700

    Add files via upload

---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random generator = new Random();
+            int number = generator.Next(1, 100);
+            label1.Visible = true;
+
+            if (textBox1.Text.Length == 0)
+            {
+                label1.ForeColor = Color.Blue;
+                label1.Text = "Сначала введите вопрос!";
+            }
+            else
+            {
+                if (number < 60)
+                {
+                    label1.ForeColor = Color.Green;
+                    label1.Text = "Да!";
+                }
+                else
+                {
+                    label1.ForeColor = Color.IndianRed;
+                    label1.Text = "Нет!";
+                }
+            }
+        }
+    }
+}
